710e869ccd845559055b83f8e9d018d57188776e
Antonio Lopez <pezlop202@gmail.com>
2022-10-09 15:23:44 -0700

    Add footer and navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { FaGithub, FaLinkedin, FaGlobeAmericas } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className='bg-msuGreen py-6'>
+      <div className='flex flex-col items-center text-sm text-softWhite'>
+        <div>
+          <span>API by </span>
+          <a
+            className='text-red'
+            href='https://www.nps.gov/subjects/developer/index.htm'
+          >
+            National Parks Service
+          </a>
+        </div>
+        <span>Designed by Antonio Lopez</span>
+        <div className='flex space-x-4 pt-5'>
+          <FaGithub className='h-7 w-7' />
+          <FaLinkedin className='h-7 w-7' />
+          <FaGlobeAmericas className='h-7 w-7' />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
+import { FaTree, FaSearch } from 'react-icons/fa';
+
 const Navbar = () => {
   return (
-    <nav class='bg-green-800 py-5'>
-      <div class='layout flex items-center justify-between'>
-        <div class='flex'>
-          <a className='text-2xl' href='/'>
+    <nav className='bg-msuGreen py-4'>
+      <div className='layout flex items-center justify-between'>
+        <div className='flex items-center'>
+          <FaTree className='h-5 w-5 text-sheenGreen' />
+          <a className='pr-3 text-softWhite' href='/'>
             What The Park?
           </a>
+          <a className='text-red' href='/'>
+            About
+          </a>
+        </div>
+        <div className='rounded-xl bg-white p-2'>
+          <FaSearch className='h-5 w-5' />
         </div>
       </div>
     </nav>
