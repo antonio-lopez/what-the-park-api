c56376b4d79642f5603e745d46b0458505e9abd1
Antonio Lopez <pezlop202@gmail.com>
2022-10-09 16:04:51 -0700

    Fix external links and stlying

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaGithub, FaLinkedin, FaGlobeAmericas } from 'react-icons/fa/index';
 
 const Footer = () => {
   return (
-    <footer className='bg-msuGreen py-6'>
+    <footer className='bg-msuGreen py-5'>
       <div className='flex flex-col items-center text-sm text-softWhite'>
         <div>
           <span>API by </span>
@@ -15,9 +15,33 @@ const Footer = () => {
         </div>
         <span>Designed by Antonio Lopez</span>
         <div className='flex space-x-4 pt-5'>
-          <FaGithub className='h-7 w-7' />
-          <FaLinkedin className='h-7 w-7' />
-          <FaGlobeAmericas className='h-7 w-7' />
+          <a
+            className='hover:text-sheenGreen'
+            href='https://github.com/antonio-lopez'
+            aria-label='Antonio Lopez github profile'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub className='h-7 w-7' />
+          </a>
+          <a
+            className='hover:text-sheenGreen'
+            href='https://www.linkedin.com/in/antoniolopez6/'
+            aria-label='Antonio Lopez linkedin profile'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin className='h-7 w-7' />
+          </a>
+          <a
+            className='hover:text-sheenGreen'
+            href='https://www.antoniolopez.me/'
+            aria-label='Antonio Lopez website'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGlobeAmericas className='h-7 w-7' />
+          </a>
         </div>
       </div>
     </footer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { FaTree, FaSearch } from 'react-icons/fa/index';
 
 const Navbar = () => {
   return (
-    <nav className='bg-msuGreen py-4'>
+    <nav className='bg-msuGreen py-3'>
       <div className='layout flex items-center justify-between'>
         <div className='flex items-center'>
-          <FaTree className='h-5 w-5 text-sheenGreen' />
+          <FaTree className='h-6 w-6 pb-1 text-sheenGreen' />
           <a className='pr-3 text-softWhite' href='/'>
             What The Park?
           </a>
@@ -13,8 +13,8 @@ const Navbar = () => {
             About
           </a>
         </div>
-        <div className='rounded-xl bg-white p-2'>
-          <FaSearch className='h-5 w-5' />
+        <div className='rounded-md bg-white p-2'>
+          <FaSearch className='h-4 w-4' />
         </div>
       </div>
     </nav>
