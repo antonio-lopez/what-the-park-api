0a6766f7c1cd1ac3d904f21938c625c5972352fd
Antonio Lopez <pezlop202@gmail.com>
2022-10-09 15:42:45 -0700

    Add card list and card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+import { FaMapMarkerAlt } from 'react-icons/fa';
+
+const Card = () => {
+  return (
+    <div className='relative shadow-2xl'>
+      {/* text box */}
+      <div className='absolute bottom-0 w-full p-1 text-xs'>
+        <div className='bg-white p-0.5'>
+          <h1>Abraham Lincoln Birthplace</h1>
+          <div className='flex justify-end'>
+            <FaMapMarkerAlt className='h-3 w-3 text-black' />
+            <span>KY</span>
+          </div>
+        </div>
+      </div>
+      {/* image */}
+      <img src='/images/park-sample.jpg' alt='park cover' />
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Park.jsx
@@ -0,0 +1,34 @@
+import Card from './Card';
+
+const Park = () => {
+  return (
+    <section className='min-h-main bg-softWhite'>
+      <div className='layout'>
+        <div className='grid grid-cols-2 gap-5 py-7'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Park;
