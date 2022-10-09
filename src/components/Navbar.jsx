bcbc495ff7c56fde51dfb1ff247d2c31bce3b205
Antonio Lopez <pezlop202@gmail.com>
2022-10-09 13:45:50 -0700

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav class='bg-green-800 py-5'>
+      <div class='layout flex items-center justify-between'>
+        <div class='flex'>
+          <a className='text-2xl' href='/'>
+            What The Park?
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
