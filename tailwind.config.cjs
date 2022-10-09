0602f6653c6fd1529ac5ce0ce666c8922e74d427
Antonio Lopez <pezlop202@gmail.com>
2022-10-09 14:30:08 -0700

    Add custom stlying and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        softWhite: '#F5F5F5',
+        msuGreen: '#243B35',
+        red: '#FE0B0B',
+        darkSlateBlue: '#4E4A99',
+        sheenGreen: '#62D905',
+      },
+    },
+  },
+  plugins: [],
+};
