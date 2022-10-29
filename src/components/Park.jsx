6855dc3ece1544111a7462d8844bb0bc106941a0
Antonio Lopez <pezlop202@gmail.com>
2022-10-28 18:24:17 -0700

    Fix Astro build

--- src/components/Park.jsx
+++ src/components/Park.jsx
@@ -1,12 +1,11 @@
 import Card from './Card';
 
 const Park = ({ parks }) => {
-  const data = parks.data;
   return (
     <section className='min-h-main bg-softWhite'>
       <div className='layout'>
         <div className='grid grid-cols-2 gap-3 py-7'>
-          {data.map((park) => (
+          {parks.map((park) => (
             <Card park={park} />
           ))}
         </div>
